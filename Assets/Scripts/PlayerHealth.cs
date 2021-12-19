268b19424028655a148a3110f53dd7e1e4c889b0
shw4455 <74192764+shw4455@users.noreply.github.com>
2021-12-20 00:47:59 +0900

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,7 +2,8 @@
 using UnityEngine.UI; // UI 관련 코드
 
 // 플레이어 캐릭터의 생명체로서의 동작을 담당
-public class PlayerHealth : LivingEntity {
+public class PlayerHealth : LivingEntity
+{ // LivingEntity을 상속
     public Slider healthSlider; // 체력을 표시할 UI 슬라이더
 
     public AudioClip deathClip; // 사망 소리
@@ -15,34 +16,92 @@ public class PlayerHealth : LivingEntity {
     private PlayerMovement playerMovement; // 플레이어 움직임 컴포넌트
     private PlayerShooter playerShooter; // 플레이어 슈터 컴포넌트
 
-    private void Awake() {
+    private void Awake()
+    {
         // 사용할 컴포넌트를 가져오기
+        playerAnimator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
+
+        // 플레이어 사망시 스크립트/컴포넌트를 비활성화 하기 위함
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
     }
 
-    protected override void OnEnable() {
+    protected override void OnEnable()
+    {
         // LivingEntity의 OnEnable() 실행 (상태 초기화)
-        base.OnEnable();
+        base.OnEnable(); // 부모 클래스에 있는 OnEnable() 메서드를 그대로 사용
+
+        // 체력 슬라이더 활성화
+        healthSlider.gameObject.SetActive(true);
+        // 체력 슬라이더의 최대값을 기본 체력값으로 변경
+        healthSlider.maxValue = startingHealth;
+        // 체력 슬라이더의 값을 현재 체력값으로 변경
+        healthSlider.value = health;
+
+        // 플레이어 조작을 받는 컴포넌트 활성화
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
+        // 애니메이터와 오디오는 여기서 안 켜주나요?
+        // 책에 바로 나왔다, 부활이라는 기능을 염두해둔 구현이라고
     }
 
     // 체력 회복
-    public override void RestoreHealth(float newHealth) {
+    public override void RestoreHealth(float newHealth)
+    {
         // LivingEntity의 RestoreHealth() 실행 (체력 증가)
         base.RestoreHealth(newHealth);
+        // 갱신된 체력으로 체력 슬라이더를 갱신
+        healthSlider.value = health;
     }
 
     // 데미지 처리
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection)
+    {
+        if (!dead) // 사망하지 않은 경우에만 효과음을 재생
+        {
+            playerAudioPlayer.PlayOneShot(hitClip); // oneshot으로 해주는 이유는?, 소리가 중간에 안 끊기게 하기 위해
+        }
         // LivingEntity의 OnDamage() 실행(데미지 적용)
         base.OnDamage(damage, hitPoint, hitDirection);
+        // 갱신된 체력을 체력 슬라이더에 반영
+        healthSlider.value = health;
     }
 
     // 사망 처리
-    public override void Die() {
+    public override void Die()
+    {
         // LivingEntity의 Die() 실행(사망 적용)
         base.Die();
+
+        // 체력 슬라이더 비활성화
+        healthSlider.gameObject.SetActive(false);
+
+        // 사망을 재생
+        playerAnimator.SetTrigger("Die");
+
+        // 플레이어 조작을 받는 컴퍼논트 비활상화
+        playerMovement.enabled = false; // setactive는 게임 오브젝트를 비활성화
+        playerShooter.enabled = false; // enabled는 컴포넌트를 비활성화
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+        // 사망하지 않은 경우에만 아이템 사용 가능
+        if (!dead)
+        {
+            // 충돌한 상대방을부터 Item 컴포넌트를 가져오기 시도
+            IItem item = other.GetComponent<IItem>();
+
+            // 충돌한 상대방으로 부터의 Item 컴포넌트를 가져오는데 성공했다면
+            if (item != null)
+            {
+                // use 메서드를 실행하여 아이템을 사용
+                item.Use(gameObject); // ?
+                // 아이템 습득 소리 재생
+                playerAudioPlayer.PlayOneShot(itemPickupClip);
+            }
+        }
     }
 }
